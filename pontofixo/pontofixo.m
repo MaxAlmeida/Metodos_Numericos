ada4ee1769913c85a441ae79b91621af08ef87ae
MaxAlmeida <llewxam100@hotmail.com>
2014-08-27 18:08:08 -0300

    grafico ponto fixo

--- pontofixo/pontofixo.m
+++ pontofixo/pontofixo.m
@@ -4,16 +4,19 @@ funcao = 'sqrt(x) - x/3 + 1/9';
 funcao_iterativa = '3*sqrt(x) + 1/3';
 valorinicial = input('Digite um valor inicial: ');
 iteracoes = 1;
-precisao = 0.000001;
+precisao = 0.00001;
 x = valorinicial
 f_x = eval(funcao_iterativa)
 while ( (f_x ~= x)&&((f_x-x)>precisao) )
-     display([num2str(f_x),' ',num2str(x)]);
+     display([num2str(f_x)]);
    x = f_x;
    f_x = eval(funcao_iterativa);
-   
+   eixo_x(iteracoes) = iteracoes;
+   eixo_y(iteracoes) = x;
       
    
 iteracoes = iteracoes +1;
 
 end
+
+plot(eixo_x,eixo_y);
