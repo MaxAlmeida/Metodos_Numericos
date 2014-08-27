7e3c3ca6fa18c3f9b45e932c8f3fb1767f9e6009
MaxAlmeida <llewxam100@hotmail.com>
2014-08-27 17:54:27 -0300

    ponto fixo

--- pontofixo/pontofixo.m
+++ pontofixo/pontofixo.m
@@ -4,10 +4,10 @@ funcao = 'sqrt(x) - x/3 + 1/9';
 funcao_iterativa = '3*sqrt(x) + 1/3';
 valorinicial = input('Digite um valor inicial: ');
 iteracoes = 1;
-precisao = 0.001;
+precisao = 0.000001;
 x = valorinicial
 f_x = eval(funcao_iterativa)
-while ( f_x ~= x )
+while ( (f_x ~= x)&&((f_x-x)>precisao) )
      display([num2str(f_x),' ',num2str(x)]);
    x = f_x;
    f_x = eval(funcao_iterativa);
